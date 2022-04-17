d73cb645b3786e835bbd479f47e038c0027f5485
spoorn <spookump@gmail.com>
2022-04-17 02:46:40 -0700

    Fix files being copied incorrectly as all directories

--- src/main/java/org/spoorn/simplebackup/util/SimpleBackupUtil.java
+++ src/main/java/org/spoorn/simplebackup/util/SimpleBackupUtil.java
@@ -3,11 +3,16 @@ package org.spoorn.simplebackup.util;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Set;
 
 @Log4j2
 public class SimpleBackupUtil {
+    
+    private static final Set<String> FILES_TO_SKIP_COPY = Set.of(
+            "session.lock"
+    );
     
     public static void createDirectoryFailSafe(Path path) {
         try {
@@ -18,20 +23,27 @@ public class SimpleBackupUtil {
     }
     
     public static boolean copyDirectoriesFailSafe(Path source, Path destination) {
-        String srcPathStr = source.toString();
         try {
-            Files.walk(source)
-                    .forEach(src -> {
-                        Path dest = Path.of(destination.toString(), src.toString().substring(srcPathStr.length()));
-                        try {
-                            if (Files.notExists(dest)) {
-                                Files.copy(source, dest);
-                            }
-                        } catch (IOException e) {
-                            log.error(String.format("Could not copy file source=%s to destination=%s", src, dest), e);
-                            throw new RuntimeException(e);
-                        }
-                    });
+            Files.walkFileTree(source, new SimpleFileVisitor<>() {
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                        throws IOException {
+                    Files.createDirectories(destination.resolve(source.relativize(dir)));
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    Path dest = destination.resolve(source.relativize(file));
+                    if (!FILES_TO_SKIP_COPY.contains(file.getFileName().toString()) && Files.notExists(dest)) {
+                        Files.copy(file, dest, StandardCopyOption.COPY_ATTRIBUTES);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            
             return true;
         } catch (Exception e) {
             log.error(String.format("Could not copy directory from source=%s to destination=%s", source, destination), e);

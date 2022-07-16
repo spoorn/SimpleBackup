6fc57a24a0901c61fae49abf859a98bd4596a54b
spoorn <spookump@gmail.com>
2022-07-16 03:01:04 -0700

    Split file index intervals by sliced directory size to better balance load across threads for multi-threaded LZ4 compression

--- src/main/java/org/spoorn/simplebackup/util/SimpleBackupUtil.java
+++ src/main/java/org/spoorn/simplebackup/util/SimpleBackupUtil.java
@@ -20,9 +20,8 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Log4j2
@@ -65,6 +64,64 @@ public class SimpleBackupUtil {
         return Files.walk(path)
                 .filter(p -> !p.toFile().isDirectory())
                 .count();
+    }
+    
+    public static long getDirectorySize(Path path) throws IOException {
+        AtomicLong size = new AtomicLong();
+
+        // Ignores failed files, should be fine for our use case
+        Files.walkFileTree(path, new SimpleFileVisitor<>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                size.addAndGet(attrs.size());
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        
+        return size.get();
+    }
+
+    /**
+     * Scans through a directory and finds the file count intervals, meaning the file number while walking through the
+     * path, that split all the files evenly by size.  For balancing multi-threaded processing of a directory recursively. 
+     * 
+     * @param path Path to process
+     * @param numIntervals Number of intervals
+     * @return long[] that holds the file number indexes to split at
+     * @throws IOException If processing files fail
+     */
+    public static long[] getFileCountIntervalsFromSize(Path path, int numIntervals) throws IOException {
+        long[] res = new long[numIntervals];
+        // index of res, file count, current size, previous size
+        long[] state = {1, 0, 0, 0};
+        long sliceLength = getDirectorySize(path) / numIntervals;
+        
+        Files.walkFileTree(path, new SimpleFileVisitor<>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (state[0] < res.length) {
+                    state[2] += attrs.size();
+                    if (state[3] / sliceLength < state[2] / sliceLength) {
+                        res[(int) state[0]] = state[1];
+                        state[0]++;
+                    }
+                    state[3] = state[2];
+                    state[1]++;
+                    return FileVisitResult.CONTINUE;
+                } else {
+                    return FileVisitResult.TERMINATE;   
+                }
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                state[1]++;
+                return super.visitFileFailed(file, exc);
+            }
+        });
+        return res;
     }
     
     public static boolean backup(Path source, String worldFolderName, String timeStr, String backupFormat) {
